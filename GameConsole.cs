e2d19c95a0d90d45dbda58dec97fba21691e6603
Sebastian <mrseb10@gmail.com>
2024-04-06 05:49:13 +0200

    Game.Run method implementation

---
+++ GameConsole.cs
@@ -0,0 +1,15 @@
+﻿namespace MathGame.BBualdo
+{
+  internal class GameConsole
+  {
+    public static void ShowTitle()
+    {
+      Console.WriteLine("------ Math Game ------");
+    }
+
+    public static void ShowError(string message)
+    {
+      Console.WriteLine(message);
+    }
+  }
+}
--- Models/Game.cs
+++ Models/Game.cs
@@ -17,5 +17,73 @@ namespace MathGame.BBualdo.Models
       DifficultyLevel = difficulty;
       NumberOfQuestions = numberOfQuestions;
     }
+
+    public void Run()
+    {
+      char? currentOperator = GameEngine.CheckOperators(GameType);
+
+      for (int i = 0; i < NumberOfQuestions; i++)
+      {
+        if (GameType == GameTypes.Random)
+        {
+          // Generating new operator for each question
+          currentOperator = GameEngine.CheckOperators(GameType);
+        }
+
+        Random random = new Random();
+        int num1 = random.Next(1, 10);
+        int num2 = random.Next(1, 10);
+
+        Console.WriteLine($"{num1} {currentOperator} {num2} ?");
+
+        string? userAnswer = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(userAnswer))
+        {
+          Console.WriteLine("Wrong answer!");
+        }
+
+        bool isCorrect = false;
+
+        if (int.TryParse(userAnswer, out int result))
+        {
+          switch (currentOperator)
+          {
+            case '+':
+              isCorrect = result == num1 + num2; break;
+            case '-':
+              isCorrect = result == num1 - num2; break;
+            case '*':
+              isCorrect = result == num1 * num2; break;
+            case '/':
+              if (num2 != 0)
+              {
+                isCorrect = result == num1 / num2;
+              }
+              break;
+            default:
+              GameConsole.ShowError("Invalid operator");
+              break;
+          }
+        }
+        else
+        {
+          GameConsole.ShowError("Wrong answer! Only numbers can be correct answer!");
+        }
+
+        if (isCorrect)
+        {
+          Console.WriteLine("Correct answer!");
+          Score++;
+        }
+        else
+        {
+          Console.WriteLine("Wrong answer!");
+        }
+      }
+
+      Console.WriteLine($"Game over! You answered correctly for {Score} out of {NumberOfQuestions} questions.");
+      // Add game to games history
+    }
   }
 }
